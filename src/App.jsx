fb39351ed35a420e237d09500542317d9d232df5
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-09 20:02:15 +0530

    updated login function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { checkUserExist, login } from './database/auth.db';
 
 function App() {
   return (
     <div className="App">
       <h1>Tracker</h1>
+      <button onClick={login}>Hello</button>
     </div>
   );
 }
--- src/database/auth.db.js
+++ src/database/auth.db.js
@@ -1,8 +1,19 @@
 import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
-import { auth, userCollection } from "./config.db";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth, db, userCollection } from "./config.db";
 
 const login = async () => {
     const user = await loginWithGoogle();
+
+    const userExist = await checkUserExist(user.uid);
+
+    console.log(user);
+
+    if (!userExist) {
+        createNewUser(user);
+    } else {
+        console.log("user logged in");
+    }
 };
 
 const loginWithGoogle = async () => {
@@ -11,9 +22,29 @@ const loginWithGoogle = async () => {
     return user;
 };
 
-const checkUserExist = async () => {
-    
-}
+const checkUserExist = async (uid) => {
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
 
+    if (docSnap.exists()) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
-export {login}
+const createNewUser = async (user) => {
+    const data = {
+        uid: user.uid,
+        photoURL: user.photoURL,
+        email: user.email,
+        name: user.displayName,
+        categories: [],
+        balance: 0,
+    };
+
+    await setDoc(doc(db, "users", user.uid), data);
+    console.log("new user created");
+};
+
+export { login, checkUserExist };
