3abba719124658bb70bb1ed8aef470c7cea05065
sujataprasad01 <sujataprasad2004@gmail.com>
2024-03-09 18:57:30 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <h1>Tracker</h1>
+      <Dashboard></Dashboard>
     </div>
   );
 }
