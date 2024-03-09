819e1c814d8a4646ce3fc876398e66ac73e70800
devmulkalwar <devmulkalwar95@gmail.com>
2024-03-09 17:10:00 +0530

    Routes added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./App"
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//pages imports
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Charts from './pages/Charts';
+import Transactions from './pages/Transactions';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error/>,
+    children : [
+      { index: true, element: <Home />},
+      { path: "login", element: <Login /> },
+      { path: "transactions", element: <Transactions /> },
+      { path: "charts", element: <Charts /> },
+    ]
+
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
