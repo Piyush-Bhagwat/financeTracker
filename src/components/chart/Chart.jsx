8b49b5fdb849106079f8f15c0736add2a6d9300b
sujataprasad01 <sujataprasad2004@gmail.com>
2024-03-10 12:57:41 +0530

    added analytics

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,55 +1,38 @@
-import React from 'react'
-import { ChartJs } from 'chart.js'
-import { Line } from 'react-chartjs-2'
-import './Chart.css'
-import moment from 'moment'
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { useGlobalContext } from '../../context/Context';
 
 function Chart() {
-
-  const dateFormat = (date) => {
-    return moment(date).format('DD/MM/YYYY')
-  }
-
-  const { incomes, expenses } = context()
+  const { incomes, expenses } = useGlobalContext();
 
   const data = {
-    labels: incomes.map((inc) => {
-      const { date } = inc
-      return dateFormat(date)
-    }),
-
+    labels: ['January', 'February', 'March'], 
     datasets: [
       {
         label: 'Income',
-        data: [
-          ...incomes.map((income) => {
-            const { amount } = income
-            return amount
-          })
-        ],
-        backgroundColor: 'green'
+        data: incomes, 
+        fill: false,
+        borderColor: 'green',
+        backgroundColor: 'green', 
+        tension:.2
       },
-
       {
-        label: 'expenses',
-        data: [
-          ...expenses.map((expense) => {
-            const { amount } = expense
-            return amount
-          })
-        ],
-        backgroundColor: 'red'
-      }
-
-    ]
-  }
+        label: 'Expenses',
+        data: expenses, 
+        fill: false,
+        borderColor: 'red',
+        backgroundColor: 'red', 
+        tension:.2
+      },
+    ],
+  };
 
   return (
-    <div className='container'>
-      <Line data={data}></Line>
-      <h1>chart</h1>
+    <div className='chart'>
+      <h1>Income and Expenses Over Time</h1>
+      <Line data={data} />
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
