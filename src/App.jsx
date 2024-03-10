25295d5d1479237b003a6838e1ca1cab28082e6f
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-10 20:01:24 +0530

    sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Bottombar from './components/Bottombar';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import BottomBar from "./components/Bottombar";
 
 // import Dashboard from './components/dashboard/Dashboard';
 
 import Dashboard from "./components/dashboard/Dashboard";
 import { addTransaction, getTransactions } from "./database/transaction.db";
+import { setBalance } from "./database/user.db";
 
 function App() {
     return (
         <div className="flex flex-col justify-center items-center w-sm">
             <Header />
             <Outlet />
-            
-            <Navbar />
+            <button
+                onClick={() =>
+                    setBalance("jFL5c73O90dglYq40RyBirnztHF3", 21234)
+                }
+            >
+                Hello
+            </button>
+            <BottomBar />
             <Dashboard></Dashboard>
         </div>
     );
