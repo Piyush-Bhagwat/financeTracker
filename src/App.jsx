1dcbdd535c30a15e280d4840a2fadb6b631496d5
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-10 19:01:37 +0530

    sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
-import Dashboard from './components/dashboard/Dashboard';
-
+import Dashboard from "./components/dashboard/Dashboard";
+import { addTransaction, getTransactions } from "./database/transaction.db";
 
 function App() {
-  return (
-    <div className='flex flex-col justify-center items-center w-sm'>
-     <Header/>
-     < Outlet/>
-     < Navbar/>
-      <Dashboard></Dashboard>
-
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-center items-center w-sm">
+            <Header />
+            <Outlet />
+            
+            <Navbar />
+            <Dashboard></Dashboard>
+        </div>
+    );
 }
 
 export default App;
