7597a0bbf63522720f468cb4493c891354c0a734
sujataprasad01 <sujataprasad2004@gmail.com>
2024-03-10 15:46:53 +0530

    uncommented

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { useGlobalContext } from '../../context/Context';
-
+import moment from 'moment/moment';
 function Chart() {
-  // const dateFormat = (date) => {
-  //   return moment(date).format('DD/MM/YYYY')
-  // }
+  const dateFormat = (date) => {
+    return moment(date).format('DD/MM/YYYY')
+  }
 
-  // const { incomes, expenses } = context()
+  const { incomes, expenses } = useGlobalContext()
 
-  // const data = {
-  //   labels: incomes.map((inc) => {
-  //     const { date } = inc
-  //     return dateFormat(date)
-  //   }),
+  const data = {
+    labels: incomes.map((inc) => {
+      const { date } = inc
+      return dateFormat(date)
+    }),
 
-  //   datasets: [
-  //     {
-  //       label: 'Income',
-  //       data: [
-  //         ...incomes.map((income) => {
-  //           const { amount } = income
-  //           return amount
-  //         })
-  //       ],
-  //       backgroundColor: 'green'
-  //     },
+    datasets: [
+      {
+        label: 'Income',
+        data: [
+          ...incomes.map((income) => {
+            const { amount } = income
+            return amount
+          })
+        ],
+        backgroundColor: 'green'
+      },
 
-  //     {
-  //       label: 'expenses',
-  //       data: [
-  //         ...expenses.map((expense) => {
-  //           const { amount } = expense
-  //           return amount
-  //         })
-  //       ],
-  //       backgroundColor: 'red'
-  //     }
+      {
+        label: 'expenses',
+        data: [
+          ...expenses.map((expense) => {
+            const { amount } = expense
+            return amount
+          })
+        ],
+        backgroundColor: 'red'
+      }
 
-  //   ]
-  // }
+    ]
+  }
 
   return (
     <div className='container'>
-      {/* <Line data={data}></Line> */}
+      <Line data={data}></Line>
       <h1>chart</h1>
     </div>
   );
