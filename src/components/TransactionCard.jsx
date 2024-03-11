fdece25a53765c28a8b81094f21857dae6c12446
devmulkalwar <devmulkalwar95@gmail.com>
2024-03-11 14:53:11 +0530

    improved transaction card structure  added

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -4,19 +4,24 @@ import "../assets/style/transactioncard.css";
 const TransactionCard = () => {
   return (
     <div className="transaction-card">
-      <div className="icon">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGnItOkVLxpZ_9tdde71oIyeYXzBpKowTHLw&usqp=CAU" alt="icon" />
-      </div>
-      <div className="details">
-        <div className="type-mode">
-          <span>Food</span>
-          <span>Cash</span>
+      <div className="transaction">
+        <div className="icon">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGnItOkVLxpZ_9tdde71oIyeYXzBpKowTHLw&usqp=CAU"
+            alt="icon"
+          />
         </div>
-        <span>comment</span>
+        <div className="details">
+          <span>Food</span>
+          <span>comment</span>
+        </div>
+        <div className="amount">
+          <span> + ₹ 1000</span>
+        </div>
       </div>
-      <div className="amount-time">
-        <span>₹ 1000</span>
+      <div className="time-mode">
         <span>3.14 pm</span>
+        <span>Cash</span>
       </div>
     </div>
   );
