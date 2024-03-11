9b67129e4f63765424401a71aa787d5be186a690
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-11 20:36:08 +0530

    added filter query

--- src/database/transaction.db.js
+++ src/database/transaction.db.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs } from "@firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "@firebase/firestore";
 import { db } from "./config.db";
 
 const addTransaction = async (uid, transactionData) => {
@@ -14,16 +14,38 @@ const getTransactions = async (uid) => {
 
     const transactions = [];
 
-   transactionsSnapShot.forEach((doc) => {
+    transactionsSnapShot.forEach((doc) => {
         transactions.push({ id: doc.id, ...doc.data() });
     });
 
     console.log(transactions);
-    return transactions
+    return transactions;
 };
+
+const getAllExpenses = async (uid, month, year) => {
+    const startTimestamp = new Date(year, month, 1);
+    startTimestamp.setHours(0, 0, 0, 0);
+
+    console.log(startTimestamp.getTime());
+    const endTimestamp = new Date(year, month + 1, 0); // Get last day of previous month
+    endTimestamp.setHours(23, 59, 59, 999);
+
+    const transactionsRef = collection(db, `users/${uid}/transactions`);
+    const q = query(
+        transactionsRef,
+        where("time", ">=", startTimestamp.getTime()),
+        where("time", "<=", endTimestamp.getTime())
+    );
+
+    const docs = (await getDocs(q)).docs;
+
+    console.log(docs);
+};
+
+const getAllIncome = async () => {};
 
 const fuckIt = () => {
     alert("fuck it boi hehe");
 };
 
-export { addTransaction, getTransactions, fuckIt };
+export { addTransaction, getTransactions, getAllExpenses, fuckIt };
