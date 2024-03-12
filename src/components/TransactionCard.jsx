66da1f7e7bec1ed3f0afd7d1a0215021ce175d70
devmulkalwar <devmulkalwar95@gmail.com>
2024-03-12 17:18:59 +0530

    ....

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -3,60 +3,64 @@ import "../assets/style/transactionCard.css";
 import { useGlobalContext } from "../context/Context";
 
 const TransactionCard = ({ type, mode, category, amount, note, time }) => {
-    const { categories } = useGlobalContext();
-    let color, emoji, categoryName;
+  const { categories } = useGlobalContext();
+  let color, emoji, categoryName;
 
-    const readCategory = () => {
-        const cat = categories?.find((cat) => cat.id === category);
-        categoryName = cat?.name;
-        emoji = cat?.emoji;
-        color = cat?.color;
-    };
+  const readCategory = () => {
+    const cat = categories?.find((cat) => cat.id === category);
+    categoryName = cat?.name;
+    emoji = cat?.emoji;
+    color = cat?.color;
+  };
 
-    const getTime = () => {
-        const date = new Date(time);
-        const hour = date.getHours();
-        const min = date.getMinutes();
-        time = `${hour % 12 !== 0 ? hour % 12 : "12"}:${min < 10 ? "0" + min : min} ${hour >= 12 ? "pm" : "am"}`;
-    };
+  const getTime = () => {
+    const date = new Date(time);
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    time = `${hour % 12 !== 0 ? hour % 12 : "12"}:${
+      min < 10 ? "0" + min : min
+    } ${hour >= 12 ? "pm" : "am"}`;
+  };
 
-    getTime();
-    readCategory();
+  getTime();
+  readCategory();
 
-    return (
-        <div className="transaction-card">
-            <div
-                className="icon"
-                style={{
-                    backgroundColor: type === "income" ? "#2a8c2a" : color,
-                }}
-            >
-                {type === "income" ? "🤑" : emoji}
-            </div>
+  return (
+    <div className="transaction-card">
+      <div
+        className="icon"
+        style={{
+          backgroundColor: type === "income" ? "#2a8c2a" : color,
+        }}
+      >
+        {type === "income" ? "🤑" : emoji}
+      </div>
 
-            <div className="details">
-                <div className="up">
-                    <span className="cat">
-                        {type === "income" ? "Income" : categoryName}
-                    </span>
-                    <span className="amount">
-                        <span className="mode">{mode}</span>
-                        <span
-                            style={{
-                                color: type === "income" ? "#2a8c2a" : "#e94040",
-                            }}
-                        >
-                            {`${type === "income" ? "+" : "-"}${amount}`}
-                        </span>
-                    </span>
-                </div>
-                <div className="down">
-                    <span className="note">{note}</span>
-                    <span className="time">{time}</span>
-                </div>
-            </div>
+      <div className="details">
+        <div className="up">
+          <span className="cat">
+            {type === "income" ? "Income" : categoryName}
+          </span>
+          <span className="mode">{mode}</span>
         </div>
-    );
+        <div className="down">
+          <span className="note">{note}</span>
+        </div>
+      </div>
+
+      <div className="amount">
+        <span
+            className="amt"
+          style={{
+            color: type === "income" ? "#2a8c2a" : "#e94040",
+          }}
+        >
+          {`${type === "income" ? "+" : "-"}₹${amount}`}
+        </span>
+        <span className="time">{time}</span>
+      </div>
+    </div>
+  );
 };
 
 export default TransactionCard;
