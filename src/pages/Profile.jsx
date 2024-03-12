3063c7b062ef06e8ff35cbb074567caa9d6da7f8
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-12 10:55:20 +0530

    added categories to profile page and logot btn

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "../context/Context";
+import "../assets/style/profile.css";
+import { IoIosAdd } from "react-icons/io";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+    const { categories } = useGlobalContext();
 
-export default Profile
+    const renderCategories = () => {
+        return (
+            <>
+                {categories?.map((cat) => {
+                    return (
+                        <div className="category">
+                            <span
+                                className="emoji"
+                                style={{ backgroundColor: cat.color }}
+                            >
+                                {cat.emoji}
+                            </span>
+                            {cat.name}
+                        </div>
+                    );
+                })}
+            </>
+        );
+    };
+
+    return (
+        <div className="profile-page">
+            <h2>Your Profile</h2>
+
+            <h3>All Categories</h3>
+            <div className="categories">
+                {renderCategories()}
+                <button className="category">
+                    <span className="emoji">
+                        <IoIosAdd />
+                    </span>
+                    Add
+                </button>
+            </div>
+
+            <h3>Logout</h3>
+            <button className="logout-btn">Logout</button>
+        </div>
+    );
+};
+
+export default Profile;
