18434c1c9a750d0906d983ce7bbec8e7771e6d21
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-03-19 16:08:00 +0530

    fixed time issue with transaction

--- src/pages/AddTransaction.jsx
+++ src/pages/AddTransaction.jsx
@@ -7,153 +7,153 @@ import { useNavigate } from "react-router-dom";
 import DatePicker from "../components/DatePicker";
 
 const AddTransaction = () => {
-  const navigate = useNavigate();
-  const { categories, user, date ,time, setTime } = useGlobalContext();
-  const [showCalBtn, setShowCalBtn] = useState(true);
-  const [amount, setAmount] = useState("");
-  const [comment, setComment] = useState("");
-  const [paymentMode, setPaymentMode] = useState("cash");
-  const [transactionCategory, setTransactionCategory] = useState(
-    categories?.at(0)?.id
-  );
-  const [transactionType, setTransactionType] = useState("expense");
+    const navigate = useNavigate();
+    const { categories, user, date, time, setTime } = useGlobalContext();
+    const [showCalBtn, setShowCalBtn] = useState(true);
+    const [amount, setAmount] = useState("");
+    const [comment, setComment] = useState("");
+    const [paymentMode, setPaymentMode] = useState("cash");
+    const [transactionCategory, setTransactionCategory] = useState(
+        categories?.at(0)?.id
+    );
+    const [transactionType, setTransactionType] = useState("expense");
 
-  
-
-  const handleNumberClick = (value) => {
-    setAmount(amount + value);
-  };
-
-  const handleBackspace = () => {
-    setAmount(amount.slice(0, -1));
-  };
-
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
-
-  const handleClear = () => {
-    setAmount("");
-  };
-
- 
-
-  const handleSubmit = async () => {
-    if (amount === "") {
-      alert("Paise kon dalega !!");
-      return;
-    }
-    // Submit the expense data
-    const t = new Date(date?.getTime());
-    const [hour, min] = time.split(":").map(Number);
-
-    t.setHours(hour);
-    t.setMinutes(min);
-
-    const data = {
-      amount: parseFloat(amount),
-      mode: paymentMode,
-      note: comment,
-      type: transactionType,
-      time: t.getTime(),
-      category: transactionCategory,
+    const handleNumberClick = (value) => {
+        setAmount(amount + value);
     };
 
-    await addTransaction(user?.uid, data);
-    console.log(`added a transaction`);
+    const handleBackspace = () => {
+        setAmount(amount.slice(0, -1));
+    };
 
-    // Reset the fields
-    setAmount("");
-    setComment("");
-    navigate("/transactions");
-  };
+    const handleCommentChange = (event) => {
+        setComment(event.target.value);
+    };
 
-  const renderCatergories = () => {
+    const handleClear = () => {
+        setAmount("");
+    };
+
+    const handleSubmit = async () => {
+        if (amount === "") {
+            alert("Paise kon dalega !!");
+            return;
+        }
+        // Submit the expense data
+        let t = new Date(date?.getTime());
+        if (time != "") {
+            const [hour, min] = time.split(":").map(Number);
+            t.setHours(hour);
+            t.setMinutes(min);
+        } else {
+            t = new Date();
+        }
+        console.log(t);
+
+        const data = {
+            amount: parseFloat(amount),
+            mode: paymentMode,
+            note: comment,
+            type: transactionType,
+            time: t.getTime(),
+            category: transactionCategory,
+        };
+
+        await addTransaction(user?.uid, data);
+        console.log(`added a transaction`, data);
+
+        // Reset the fields
+        setAmount("");
+        setComment("");
+        navigate("/transactions");
+    };
+
+    const renderCatergories = () => {
+        return (
+            <>
+                {categories?.map((cat) => {
+                    return (
+                        <option key={cat.id} value={cat.id}>
+                            {cat.name}
+                        </option>
+                    );
+                })}
+            </>
+        );
+    };
+
+    const getColor = () => {
+        const obj = categories?.find((cat) => cat.id === transactionCategory);
+        return obj?.color;
+    };
+
+    const handleTimeChange = (e) => {
+        console.log(e.target.value);
+        setTime(e.target.value);
+    };
+
     return (
-      <>
-        {categories?.map((cat) => {
-          return (
-            <option key={cat.id} value={cat.id}>
-              {cat.name}
-            </option>
-          );
-        })}
-      </>
-    );
-  };
+        <div className="add-transaction">
+            <div className="dropdowns">
+                <select
+                    className="transaction-dropdown"
+                    value={paymentMode}
+                    onChange={(e) => setPaymentMode(e.target.value)}
+                >
+                    <option value="cash">Cash</option>
+                    <option value="card">Card</option>
+                    <option value="upi">Upi</option>
+                    <option value="netbanking">Netbanking</option>
+                </select>
 
-  const getColor = () => {
-    const obj = categories?.find((cat) => cat.id === transactionCategory);
-    return obj?.color;
-  };
+                <select
+                    className="category-dropdown"
+                    value={transactionCategory}
+                    style={{ backgroundColor: getColor() }}
+                    onChange={(e) => setTransactionCategory(e.target.value)}
+                >
+                    {renderCatergories()}
+                </select>
+            </div>
+            <div className="second-row">
+                <div className="transaction-type">
+                    <select
+                        className="type-dropdown"
+                        value={transactionType}
+                        onChange={(e) => setTransactionType(e.target.value)}
+                    >
+                        <option value="expense">Expense</option>
+                        <option value="income">Income</option>
+                    </select>
+                </div>
+            </div>
 
-  const handleTimeChange = (e) => {
-    console.log(e.target.value);
-    setTime(e.target.value);
-  };
+            <div className="amount-entry">
+                <input
+                    type="text"
+                    className="amount-input"
+                    value={`₹${amount}`}
+                    readOnly
+                />
+                <input
+                    type="text"
+                    className="comment-input"
+                    placeholder="Add comment..."
+                    value={comment}
+                    onChange={handleCommentChange}
+                />
+            </div>
 
-  return (
-    <div className="add-transaction">
-      <div className="dropdowns">
-        <select
-          className="transaction-dropdown"
-          value={paymentMode}
-          onChange={(e) => setPaymentMode(e.target.value)}
-        >
-          <option value="cash">Cash</option>
-          <option value="card">Card</option>
-          <option value="upi">Upi</option>
-          <option value="netbanking">Netbanking</option>
-        </select>
-
-        <select
-          className="category-dropdown"
-          value={transactionCategory}
-          style={{ backgroundColor: getColor() }}
-          onChange={(e) => setTransactionCategory(e.target.value)}
-        >
-          {renderCatergories()}
-        </select>
-      </div>
-      <div className="second-row">
-        <div className="transaction-type">
-          <select
-            className="type-dropdown"
-            value={transactionType}
-            onChange={(e) => setTransactionType(e.target.value)}
-          >
-            <option value="expense">Expense</option>
-            <option value="income">Income</option>
-          </select>
+            <Keypad
+                handleNumberClick={handleNumberClick}
+                handleBackspace={handleBackspace}
+                handleClear={handleClear}
+                handleSubmit={handleSubmit}
+                showCalBtn={showCalBtn}
+                setShowCalBtn={setShowCalBtn}
+            />
         </div>
-      </div>
-
-      <div className="amount-entry">
-        <input
-          type="text"
-          className="amount-input"
-          value={`₹${amount}`}
-          readOnly
-        />
-        <input
-          type="text"
-          className="comment-input"
-          placeholder="Add comment..."
-          value={comment}
-          onChange={handleCommentChange}
-        />
-      </div>
-
-      <Keypad
-        handleNumberClick={handleNumberClick}
-        handleBackspace={handleBackspace}
-        handleClear={handleClear}
-        handleSubmit={handleSubmit}
-        showCalBtn={showCalBtn}
-        setShowCalBtn={setShowCalBtn}
-      />
-    </div>
-  );
+    );
 };
 
 export default AddTransaction;
